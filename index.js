31ec90cc21a611ac54e03d0d606514a8cadbee26
Ненад Абрамовић <nenadabramovic@outlook.com>
2019-11-30 17:16:19 +0100

    Исправљене грешке

--- index.js
+++ index.js
@@ -1,57 +1,91 @@
+let canvasSize = 500;
 const app = document.querySelector('#app');
+app.clientWidth = `${canvasSize}px`;
+app.clientHeight = `${canvasSize}px`;
+app.style.width = `${canvasSize}px`;
+app.style.height = `${canvasSize}px`;
+let ctx = app.getContext('2d');
 const counter = document.querySelector('#counter');
 let size = Number(prompt());
-let t = 300;
+let t = 3000;
 
-app.style.width = app.style.height = `${500 + size * 2}px`;
-
-let cells = [];
-
-for (let i = 0; i < size; i++) {
-  cells.push([]);
-  for (let j = 0; j < size; j++) {
-    let cell = document.createElement('div');
-    cell.className = 'cell';
-    cell.style.width = cell.style.height = `${500 / size}px`;
-    cells[i].push([cell, (Math.random() < 0.9) ? 'dead' : 'alive']);
-    cells[i][j][0].style.backgroundColor = (cells[i][j][1] == 'dead') ? 'black' : 'white';
-    app.appendChild(cell);
+class Cell {
+  constructor(size) {
+    this.width = canvasSize / size;
+    this.height = canvasSize / size;
+    this.state = (Math.random() < 0.90) ? 'dead' : 'alive';
+    this.newState = (Math.random() < 0.90) ? 'dead' : 'alive';
+    this.color = this.state == 'dead' ? 'black' : 'white';
+  }
+  getState() {
+    return this.state;
+  }
+  setNewState(newState) {
+    this.newState = newState;
+  }
+  updateState() {
+    this.state = this.newState;
+    this.color = this.state == 'dead' ? 'black' : 'white';
+  }
+  draw(ctx, x, y) {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.fillRect(x, y, this.width, this.height);
   }
 }
+
+
+let grid = [];
+
+for (let i = 0; i < size; i++) {
+  grid.push([]);
+  for (let j = 0; j < size; j++) {
+    grid[i].push(new Cell(size));
+  }
+}
+
 function loop() {
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
+  grid.forEach((row, i) => {
+    row.forEach((cell, j) => {
       let countLiveNeighbours = 0;
       for (let k = -1; k <= 1; k++) {
         for (let l = -1; l <= 1; l++) {
-          if (cells[i + k] != undefined) {
-            if (cells[i + k][j + l] != undefined) {
-              if (cells[i + k][j + l][1] == 'alive') {
-                countLiveNeighbours++;
-              }
-            }
+          let x = i + k;
+          let y = j + l;
+          if (x == -1) x = size - 1;
+          if (x == size) x = 0;
+
+          if (y == -1) y = size - 1;
+          if (y == size) y = 0;
+
+          if (grid[x][y].getState() == 'alive') {
+            countLiveNeighbours++;
           }
         }
       }
-      if (cells[i][j][1] == 'alive') {
+      if (cell.getState() == 'alive') {
         countLiveNeighbours--;
         if (countLiveNeighbours < 2 || countLiveNeighbours > 3) {
-          cells[i][j][1] = 'dead';
-          cells[i][j][0].style.backgroundColor = 'black';
+          cell.setNewState('dead');
         }
       } else {
         if (countLiveNeighbours == 3) {
-          cells[i][j][1] = 'alive';
-          cells[i][j][0].style.backgroundColor = 'white';
+          cell.setNewState('alive');
         }
       }
-    }
-  }
+    });
+  });
+  grid.forEach((row, i) => {
+    row.forEach((cell, j) => {
+      cell.updateState();
+      cell.draw(ctx, i * (canvasSize / size), j * (canvasSize / size));
+    });
+  });
 }
+
 let time = 0;
 let interval = setInterval(() => {
-  time++;
-  counter.textContent = `${time}/${t}`;
+  counter.textContent = `${++time}/${t}`;
   loop();
   if (time == t) clearInterval(interval);
-}, 500);
+}, 100);
